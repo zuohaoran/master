2a3a42d1e97c6a4bc0ce158989a03f6bafe3613c
zuohaoran <112619196@qq.com>
2016-02-26 17:05:58 +0800

    add framework

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=101.4.19.83;dbname=nbos',
+    'username' => 'root',
+    'password' => 'cernet2113',
+    'charset' => 'utf8',
+];
